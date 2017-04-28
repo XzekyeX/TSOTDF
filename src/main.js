55b0ceb36f36211f1af3902a2562ad2dedddcd8d
Zekye <Zekye@live.fi>
2017-04-28 19:55:22 +0300

    Trying to load mesh files

--- src/main.js
+++ src/main.js
@@ -7,49 +7,49 @@ var width = 640;
 var height = 480;
 var blue = 1.0;
 var dir = 0;
-function init(){
-	var game = $("<canvas width=\""+width+"\" height=\""+height+"\" />");
-	$("#game").append(game);
-	var engine = new BABYLON.Engine(game[0],true);
-	window.addEventListener('resize', resizeCanvas, false);
+function init() {
+    var game = $("<canvas width=\"" + width + "\" height=\"" + height + "\" />");
+    $("#game").append(game);
+    var engine = new BABYLON.Engine(game[0], true);
+    window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
-		width = game[0].width = window.innerWidth;
-		height = game[0].height = window.innerHeight;		
-		engine.resize();
+        width = game[0].width = window.innerWidth;
+        height = game[0].height = window.innerHeight;
+        engine.resize();
     }
-	resizeCanvas();
-	var scene = createScene(game[0],engine);
-	engine.runRenderLoop(function () {
-		scene.render();
-        if(dir == 0) {
+    resizeCanvas();
+    var scene = createScene(game[0], engine);
+    engine.runRenderLoop(function () {
+        scene.render();
+        if (dir == 0) {
             blue += 0.01;
-            if(blue >= 1.0){
+            if (blue >= 1.0) {
                 dir = 1;
                 console.log("dir: " + dir);
-            } 
-        } else{
+            }
+        } else {
             blue -= 0.01;
-            if(blue <= 0){
+            if (blue <= 0) {
                 dir = 0;
                 console.log("dir: " + dir);
-            } 
-        } 
-        scene.clearColor = new BABYLON.Color3(0.1,0.2,clamp(blue,0.4,1.0));
-	});
+            }
+        }
+        scene.clearColor = new BABYLON.Color3(0.1, 0.2, clamp(blue, 0.4, 1.0));
+    });
 }
 //value min max
-function clamp(value,min,max){
+function clamp(value, min, max) {
     return value >= max ? max : value <= min ? min : value;
 }
 
-function createScene(canvas,engine){
-	
+function createScene(canvas, engine) {
+
     // Now create a basic Babylon Scene object 
     var scene = new BABYLON.Scene(engine);
 
     // Change the scene background color to green.
     scene.clearColor = new BABYLON.Color3(0.1, 0.2, 1.0);
-	
+
     // This creates and positions a free camera
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
     camera.speed = 0.2;
@@ -60,7 +60,10 @@ function createScene(canvas,engine){
     camera.attachControl(canvas, false);
 
     var loader = new BABYLON.AssetsManager(scene);
-    var tree = loader.addMeshTask("DeadTree1","","../res/","DeadTree1.obj");
+    var tree = loader.addMeshTask("Skull Task", "", "res/", "skull.babylon");
+    tree.onSuccess = function (task) {
+        task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
+    }
 
     // This creates a light, aiming 0,1,0 - to the sky.
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
