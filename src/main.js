294ae4c98eaf5aa9cdd72ff2d34ab77db2127e95
Zekye <Zekye@live.fi>
2017-04-28 19:35:06 +0300

    Mesh
    Loading mesh

--- src/main.js
+++ src/main.js
@@ -52,12 +52,14 @@ function createScene(canvas,engine){
 	
     // This creates and positions a free camera
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
+    camera.speed = 0.2;
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
 
     // This attaches the camera to the canvas
     camera.attachControl(canvas, false);
+
+    BABYLON.SceneLoader.Load("res/","DeadTree1.obj",engine,scene);
 
     // This creates a light, aiming 0,1,0 - to the sky.
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
